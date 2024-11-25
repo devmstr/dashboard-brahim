172677fb12a212f58aff539b7e269fef07dc3431
devmstr <sahli.med@outlook.com>
2024-11-25 11:32:54 +0100

    make login page responsive

--- app/(login)/page.tsx
+++ app/(login)/page.tsx
@@ -16,10 +16,10 @@ export default async function LoginPage() {
   return (
     <div className="relative flex flex-col items-center justify-center h-screen w-full">
       <div className="container flex justify-center h-[80vh] max-w-5xl p-10">
-        <div className="w-1/2 flex justify-center gap-5 bg-background rounded-l-md h-full p-8  flex-col  space-y-2 drop-shadow-md">
+        <div className="w-full md:w-1/2 flex justify-center gap-5 bg-background rounded-l-md h-full p-8  flex-col  space-y-2 drop-shadow-md">
           <div className="flex flex-col space-y-2 text-start">
             <h1 className="text-2xl font-light">{'Bienvenue à'}</h1>
-            <h1 className="flex text-5xl ">
+            <h1 className="text-4xl flex md:text-5xl ">
               <strong className="font-medium">SONERAS</strong>
               <span className="font-light">Flow</span>
             </h1>
@@ -29,7 +29,7 @@ export default async function LoginPage() {
           </div>
           <UserLoginForm />
         </div>
-        <div className="w-1/2 h-full flex justify-center items-center rounded-r-md bg-primary">
+        <div className="hidden w-1/2 h-full md:flex justify-center items-center rounded-r-md bg-primary">
           <div className="flex gap-5 flex-col justify-center items-center">
             <div className="flex flex-col justify-center items-center gap-8">
               <Icons.logo className="w-36 h-auto" />
