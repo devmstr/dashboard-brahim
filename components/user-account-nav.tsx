12ca3fc7879c7ea1eb52b3e0f168c3acc6c21f93
ismail_sahli <sahli.med@outlook.com>
2024-12-03 00:38:04 +0100

    fix logout translation

--- components/user-account-nav.tsx
+++ components/user-account-nav.tsx
@@ -111,7 +111,7 @@ export const UserAccountNav: React.FC<UserAccountNavProps> = ({
         >
           <div className="flex gap-1 text-base font-medium items-center !text-gray-500 group ">
             <Icons.logout className="w-5 h-5 mr-2 group-hover:text-gray-800 " />
-            <span className=" group-hover:text-gray-800">{'logout'}</span>
+            <span className=" group-hover:text-gray-800">{'Déconnecter'}</span>
           </div>
         </DropdownMenuItem>
       </DropdownMenuContent>
