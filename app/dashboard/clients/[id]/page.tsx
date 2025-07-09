08a5af299b10ebb00fbe1411150e2b98949b0c11
ismail_sahli <sahli.med@outlook.com>
2025-07-09 09:28:10 +0100

    fix build

--- app/dashboard/clients/[id]/page.tsx
+++ app/dashboard/clients/[id]/page.tsx
@@ -1,28 +1,33 @@
-'use client'
-import { EditClientForm } from '@/app/api/clients/[id]/edit-client.form'
 import { Card } from '@/components/card'
-import { useRouter } from 'next/navigation'
+import { notFound, useRouter } from 'next/navigation'
 import { useState } from 'react'
+import { EditClientForm } from './one-client.form'
+import { formatPhoneNumber } from '@/lib/utils'
 
 // Example of how to use the EditClientForm component
-export default function EditClientPage({
+export default async function EditClientPage({
   params: { id }
 }: {
   params: { id: string }
 }) {
-  const [isEditing, setIsEditing] = useState(false)
-  const clientId = id
-  const router = useRouter()
-
-  const handleSuccess = (updatedClient: any) => {
-    setIsEditing(false)
-  }
-
-  const handleCancel = () => {
-    router.push('/dashboard/clients')
-    setIsEditing(false)
-  }
-
+  const record = await prisma?.client.findFirst({
+    where: { isCompany: true },
+    orderBy: { updatedAt: 'desc' },
+    include: {
+      Address: {
+        include: {
+          Province: true,
+          Country: true,
+          City: true
+        }
+      },
+      _count: {
+        select: { Orders: true }
+      }
+    }
+  })
+  if (!record) return notFound()
+  const { Address, ...client } = record
   return (
     <Card className="space-y-6">
       <div className="">
@@ -33,9 +38,32 @@ export default function EditClientPage({
       </div>
 
       <EditClientForm
-        clientId={clientId}
-        onSuccess={handleSuccess}
-        onCancel={handleCancel}
+        data={{
+          ...client,
+          label: client.label ?? undefined,
+          email: client.label ?? undefined,
+          phone: formatPhoneNumber(client.phone),
+          website: client.website ?? undefined,
+          tradeRegisterNumber: client.tradeRegisterNumber ?? undefined,
+          registrationArticle: client.registrationArticle ?? undefined,
+          taxIdNumber: client.taxIdNumber ?? undefined,
+          fiscalNumber: client.fiscalNumber ?? undefined,
+          approvalNumber: client.approvalNumber ?? undefined,
+          name: client.name ?? undefined,
+          statisticalIdNumber: client.statisticalIdNumber ?? undefined,
+          isCompany: client.isCompany ?? undefined,
+          addressId: client.addressId ?? undefined,
+          ...(Address && {
+            street: Address.street ?? undefined,
+            cityId: Address.cityId ?? undefined,
+            provinceId: Address.provinceId ?? undefined,
+            countryId: Address.countryId ?? undefined,
+            country: Address.Country.name ?? undefined,
+            province: Address.Province.name ?? undefined,
+            city: Address.City.name ?? undefined,
+            zip: Address.City.zipCode ?? undefined
+          })
+        }}
       />
     </Card>
   )
--- app/dashboard/clients/page.tsx
+++ app/dashboard/clients/page.tsx
@@ -2,27 +2,64 @@ import { AddNewClientDialogButton } from '@/components/add-new-client.button'
 import { Card } from '@/components/card'
 import { ClientTable } from '@/components/client-table'
 import db from '@/lib/db'
+import { formatPhoneNumber } from '@/lib/utils'
+import { notFound } from 'next/navigation'
 
 interface Props {}
 
 const Page: React.FC<Props> = async ({}: Props) => {
-  const clients = await db.client.findMany({
+  const data = await db?.client.findMany({
     where: { isCompany: true },
     orderBy: { updatedAt: 'desc' },
     include: {
-      Address: { include: { Province: true } },
+      Address: {
+        include: {
+          Province: true,
+          Country: true,
+          City: true
+        }
+      },
       _count: {
         select: { Orders: true }
       }
-    }
+    },
+    take: 4
   })
+  if (!data) return notFound()
 
   return (
     <Card className="">
       <div className="flex justify-end items-center gap-3 mb-5">
         <AddNewClientDialogButton />
       </div>
-      <ClientTable data={clients} />
+      <ClientTable
+        data={data.map(({ Address, ...client }) => ({
+          ...client,
+          label: client.label ?? undefined,
+          email: client.label ?? undefined,
+          phone: formatPhoneNumber(client.phone),
+          website: client.website ?? undefined,
+          tradeRegisterNumber: client.tradeRegisterNumber ?? undefined,
+          registrationArticle: client.registrationArticle ?? undefined,
+          taxIdNumber: client.taxIdNumber ?? undefined,
+          fiscalNumber: client.fiscalNumber ?? undefined,
+          approvalNumber: client.approvalNumber ?? undefined,
+          name: client.name ?? undefined,
+          statisticalIdNumber: client.statisticalIdNumber ?? undefined,
+          isCompany: client.isCompany ?? undefined,
+          addressId: client.addressId ?? undefined,
+          ...(Address && {
+            street: Address.street ?? undefined,
+            cityId: Address.cityId ?? undefined,
+            provinceId: Address.provinceId ?? undefined,
+            countryId: Address.countryId ?? undefined,
+            country: Address.Country.name ?? undefined,
+            province: Address.Province.name ?? undefined,
+            city: Address.City.name ?? undefined,
+            zip: Address.City.zipCode ?? undefined
+          })
+        }))}
+      />
     </Card>
   )
 }
